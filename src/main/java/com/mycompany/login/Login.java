2bcd55f85121d3daedc990676c7efa260e7d462d
Siyabonga-LNP-35 <ST10453541@rcconnect.edu.za>
2024-09-23 17:02:18 +0200

    12th commit after i have run the project file

---
+++ src/main/java/com/mycompany/login/Login.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.login;
+
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author siyab
+ */
+public class Login {
+    
+    // Declarations 
+
+    private final String firstName;
+    private final String lastName;
+    private final String username;
+    private final String password;
+
+      // initialize user details
+    public Login(String firstName, String lastName, String username, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+    }
+    // Method to check the username format
+    public boolean checkUserName() {
+        return username.contains("_") && username.length() <= 5;
+    }
+    // Method to check password complexity
+    public boolean checkPasswordComplexity() {
+        boolean hasUpperCase = false, hasDigit = false, hasSpecialChar = false;
+        if (password.length() >= 8) {
+            for (char c : password.toCharArray()) {
+                if (Character.isUpperCase(c)) hasUpperCase = true;
+                if (Character.isDigit(c)) hasDigit = true;
+                if (!Character.isLetterOrDigit(c)) hasSpecialChar = true;
+            }
+            return hasUpperCase && hasDigit && hasSpecialChar;
+        }
+        return false;
+    }
+    //Method to register user 
+    
+    public String  registerUser(){
+        if (!checkUserName()){
+            return "Username is not correctly formatted, please ensure that the usename contains an underscore and is no more than 5 characters in length.";         
+        }else if (!checkPasswordComplexity()){
+            return "Password is not correctly formatted, please ensure that the password contains at least 8 characters, a capital letter, a number, and spercial character.";
+        }
+        return "Username and password successfully captured.";
+    }
+        //Method to verify User login Details 
+     public boolean loginUser(String enteredUsername, String enteredPassword){
+         return enteredUsername.equals(this.username) && enteredPassword.equals(this.password);
+         
+     }
+     //Method to return login status
+     public String returnLoginStatus(boolean isLoggedIn){
+        if (isLoggedIn) {
+            return "Welcome " + this.firstName + "  " + this.lastName + ",it is great  to see you again.";
+        }else {
+            return "Username or password incorrect, please try again.";
+            
+        }
+     }
+}
---
+++ src/main/java/com/mycompany/login/Main.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.login;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author siyab
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Scanner sc = new Scanner(System.in);
+        // Registration
+        System.out.println("Enter your first name:");
+        String firstName = sc.nextLine();
+        
+        System.out.println("Enter your last name:");
+        String lastName = sc.nextLine();
+        
+        System.out.println("Enter your username (must contain an underscore and be no more than 5 characters):");
+        String username = sc.nextLine();
+        
+        System.out.println("Enter your password (must be at least 8 characters, contain a capital letter, a number, and a special character):");
+        String password = sc.nextLine();
+        
+        Login user = new Login(firstName, lastName, username, password);
+        
+        String registrationMessage = user.registerUser();
+        System.out.println(registrationMessage);
+        
+        if (registrationMessage.equals("Username and password successfully captured.")) {
+            // Login
+            System.out.println("Enter your username to log in:");
+            String enteredUsername = sc.nextLine();
+            
+            System.out.println("Enter your password:");
+            String enteredPassword = sc.nextLine();
+            
+            boolean isLoggedIn = user.loginUser(enteredUsername, enteredPassword);
+            System.out.println(user.returnLoginStatus(isLoggedIn));
+              
+          }
+        
+    }
+}
